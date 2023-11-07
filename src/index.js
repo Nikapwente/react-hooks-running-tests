4bdfce87423e026da9489b855a33b66d03e3420b
Nikapwente <hrsmajor@gmail.com>
2023-11-07 07:01:56 +0300

    Assignment complete

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from "./components/App";
 import "./index.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
+
+let headerElement = document.createElement('header');
+headerElement.value = "hello from the Header!";
+
